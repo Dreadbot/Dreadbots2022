53af7e400b96aff6f0afb039499271815cc452b4
cjohnson <collin.g.johnson@gmail.com>
2022-01-15 21:11:21 -0500

    Add basic test class for Drive

---
+++ robot/src/test/java/frc/robot/subsystem/DriveTest.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.*;
+
+import edu.wpi.first.hal.HAL;
+import frc.robot.subsystem.Drive;
+import frc.robot.Constants;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import org.junit.*;
+
+public class DriveTest {
+    public static final double DELTA = 1e-2;
+    
+    private Drive drive;
+    private CANSparkMax leftFrontDriveMotor;
+    private CANSparkMax rightFrontDriveMotor;
+    private CANSparkMax leftBackDriveMotor;
+    private CANSparkMax rightBackDriveMotor;
+
+    @Before
+    public void setup() {
+        assert HAL.initialize(500, 0);
+        leftFrontDriveMotor = new CANSparkMax(Constants.LEFT_FRONT_DRIVE_MOTOR_PORT, MotorType.kBrushless);
+        rightFrontDriveMotor = new CANSparkMax(Constants.RIGHT_FRONT_DRIVE_MOTOR_PORT, MotorType.kBrushless);
+        leftBackDriveMotor = new CANSparkMax(Constants.LEFT_BACK_DRIVE_MOTOR_PORT, MotorType.kBrushless);
+        rightBackDriveMotor = new CANSparkMax(Constants.RIGHT_BACK_DRIVE_MOTOR_PORT, MotorType.kBrushless);
+    }
+
+    @After
+    public void shutdown() throws Exception {
+        leftFrontDriveMotor.close();
+        rightFrontDriveMotor.close();
+        leftBackDriveMotor.close();
+        rightBackDriveMotor.close();
+    }
+}
