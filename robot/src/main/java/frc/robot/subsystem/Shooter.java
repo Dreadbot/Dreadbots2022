56ed7cdcc93455b11d81e294c81f1c882bacbdc9
cjohnson <collin.g.johnson@gmail.com>
2022-01-18 17:41:12 -0500

    Add PID tuning code

--- robot/src/main/java/frc/robot/subsystem/Shooter.java
+++ robot/src/main/java/frc/robot/subsystem/Shooter.java
@@ -1,12 +1,21 @@
 package frc.robot.subsystem;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax.ControlType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Shooter extends Subsystem {
     private final CANSparkMax flywheelMotor;
     private final CANSparkMax hoodMotor;
     private final CANSparkMax turretMotor;
+
+    private SparkMaxPIDController pidController;
+    private RelativeEncoder encoder;
+
+    public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM;
 
     public Shooter(CANSparkMax flywheelMotor, CANSparkMax hoodMotor, CANSparkMax turretMotor) {
         super("Shooter");
@@ -14,6 +23,35 @@ public class Shooter extends Subsystem {
         this.flywheelMotor = flywheelMotor;
         this.hoodMotor = hoodMotor;
         this.turretMotor = turretMotor;
+
+        flywheelMotor.restoreFactoryDefaults();
+        pidController = flywheelMotor.getPIDController();
+        encoder = flywheelMotor.getEncoder();
+
+        kP = 6e-5; 
+        kI = 0;
+        kD = 0; 
+        kIz = 0; 
+        kFF = 0.000015; 
+        kMaxOutput = 1; 
+        kMinOutput = -1;
+        maxRPM = 5700;
+
+        pidController.setP(kP);
+        pidController.setI(kI);
+        pidController.setD(kD);
+        pidController.setIZone(kIz);
+        pidController.setFF(kFF);
+        pidController.setOutputRange(kMinOutput, kMaxOutput);
+
+        SmartDashboard.putNumber("P Gain", kP);
+        SmartDashboard.putNumber("I Gain", kI);
+        SmartDashboard.putNumber("D Gain", kD);
+        SmartDashboard.putNumber("I Zone", kIz);
+        SmartDashboard.putNumber("Feed Forward", kFF);
+        SmartDashboard.putNumber("Max Output", kMaxOutput);
+        SmartDashboard.putNumber("Min Output", kMinOutput);
+        SmartDashboard.putNumber("RPM", 3500);
     }
 
     public void setTurretAngle(double turretAngle) {
@@ -21,9 +59,32 @@ public class Shooter extends Subsystem {
     }
 
     public void shoot() {
-        double rpm = 3500.0d;
-        rpm /= 2.0d;
-        flywheelMotor.getPIDController().setReference(rpm, ControlType.kVelocity);
+        double p = SmartDashboard.getNumber("P Gain", 0);
+        double i = SmartDashboard.getNumber("I Gain", 0);
+        double d = SmartDashboard.getNumber("D Gain", 0);
+        double iz = SmartDashboard.getNumber("I Zone", 0);
+        double ff = SmartDashboard.getNumber("Feed Forward", 0);
+        double max = SmartDashboard.getNumber("Max Output", 0);
+        double min = SmartDashboard.getNumber("Min Output", 0);
+
+        if((p != kP)) { pidController.setP(p); kP = p; }
+        if((i != kI)) { pidController.setI(i); kI = i; }
+        if((d != kD)) { pidController.setD(d); kD = d; }
+        if((iz != kIz)) { pidController.setIZone(iz); kIz = iz; }
+        if((ff != kFF)) { pidController.setFF(ff); kFF = ff; }
+        if((max != kMaxOutput) || (min != kMinOutput)) { 
+        pidController.setOutputRange(min, max); 
+        kMinOutput = min; kMaxOutput = max; 
+
+        double setPoint = SmartDashboard.getNumber("RPM", 0);
+        setPoint /= 2;
+
+        pidController.setReference(setPoint, ControlType.kVelocity);
+
+        SmartDashboard.putNumber("SetPoint", setPoint);
+        SmartDashboard.putNumber("ProcessVariable", encoder.getVelocity());
+    }
+        
     }
 
     @SuppressWarnings("unused")
