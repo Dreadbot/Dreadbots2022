2a31d3d45d096538174683eabdb33023f035104a
cjohnson <collin.g.johnson@gmail.com>
2022-01-18 14:12:02 -0500

    Finish DreadbotMath Tests (add deadband testing)

--- robot/src/test/java/frc/robot/util/DreadbotMathTest.java
+++ robot/src/test/java/frc/robot/util/DreadbotMathTest.java
@@ -258,4 +258,46 @@ public class DreadbotMathTest {
         resultValueI = DreadbotMath.clampValue(-3, bottomValueI, topValueI);
         assertEquals(bottomValueI, resultValueI, DELTA);
     }
+
+    @Test
+    public void applyDeadbandToValueDoubleTest() {
+        bottomValueD = -1.0d;
+        topValueD = 1.0d;
+
+        // Regular Function Test (Within Range)
+        resultValueD = DreadbotMath.applyDeadbandToValue(0.25d, bottomValueD, topValueD, 0.0d);
+        assertEquals(0.0d, resultValueD, DELTA);
+
+        // Regular Function Test (Outside Range)
+        resultValueD = DreadbotMath.applyDeadbandToValue(-1.25d, bottomValueD, topValueD, 0.0d);
+        assertEquals(-1.25d, resultValueD, DELTA);
+    }
+
+    @Test
+    public void applyDeadbandToValueFloatTest() {
+        bottomValueF = -1.0f;
+        topValueF = 1.0f;
+
+        // Regular Function Test (Within Range)
+        resultValueF = DreadbotMath.applyDeadbandToValue(0.25f, bottomValueF, topValueF, 0.0f);
+        assertEquals(0.0f, resultValueF, DELTA);
+
+        // Regular Function Test (Outside Range)
+        resultValueF = DreadbotMath.applyDeadbandToValue(-1.25f, bottomValueF, topValueF, 0.0f);
+        assertEquals(-1.25f, resultValueF, DELTA);
+    }
+
+    @Test
+    public void applyDeadbandToValueIntegerTest() {
+        bottomValueI = -2;
+        topValueI = 2;
+
+        // Regular Function Test (Within Range)
+        resultValueI = DreadbotMath.applyDeadbandToValue(1, bottomValueI, topValueI, 0);
+        assertEquals(0, resultValueI, DELTA);
+
+        // Regular Function Test (Outside Range)
+        resultValueI = DreadbotMath.applyDeadbandToValue(-3, bottomValueI, topValueI, 0);
+        assertEquals(-3, resultValueI, DELTA);
+    }
 }
