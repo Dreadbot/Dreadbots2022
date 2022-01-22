86d6ad2b5d8b9815750b1f98efabe65351872494
pantaelaman <calvin@theophoffs.com>
2022-01-22 13:30:51 -0500

    Added YOLOv5s model skeleton

--- vision/ballyolo/find_balls.py
+++ vision/ballyolo/find_balls.py
@@ -1,2 +1,24 @@
 import cv2
 import torch
+import pandas as pd
+
+# Model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    results = model(frame)
+    df = results.pandas().xyxy[0]
+
+    for _,row in df[df.name == 'sports ball'].iterrows():
+        cv2.rectangle(frame, (int(row['xmin']), int(row['ymin'])), (int(row['xmax']), int(row['ymax'])), (0,255,0), 3)
+
+    cv2.imshow('frame', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
