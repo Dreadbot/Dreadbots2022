7cc845f595f20473fa4bcdac22f079957e1e22fe
pantaelaman <calvin@theophoffs.com>
2022-01-31 16:01:51 -0500

    YOLO deprecated, using cascade classifiers instead

--- vision/ballyolo/find_balls.py
+++ vision/ballyolo/find_balls.py
@@ -1,9 +1,10 @@
+# -- DEPRECATED --
 import cv2
 import torch
 import pandas as pd
 
 # Model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+model = torch.hub.load('ultralytics/yolov5', 'yolov5n', device='cpu', classes=32)
 
 cap = cv2.VideoCapture(0)
 
@@ -12,7 +13,7 @@ while True:
     results = model(frame)
     df = results.pandas().xyxy[0]
 
-    for _,row in df[df.name == 'sports ball'].iterrows():
+    for _,row in df.iterrows():
         cv2.rectangle(frame, (int(row['xmin']), int(row['ymin'])), (int(row['xmax']), int(row['ymax'])), (0,255,0), 3)
 
     cv2.imshow('frame', frame)
