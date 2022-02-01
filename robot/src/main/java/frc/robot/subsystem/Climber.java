f40de3a3bca3f25e6c68be0a56b8af077990b310
KCsup <kcsup05@gmail.com>
2022-02-01 17:29:54 -0500

    add climber wrapper functions

--- robot/src/main/java/frc/robot/subsystem/Climber.java
+++ robot/src/main/java/frc/robot/subsystem/Climber.java
@@ -5,7 +5,7 @@ import com.revrobotics.CANSparkMax.IdleMode;
 
 import edu.wpi.first.wpilibj.Solenoid;
 
-public class Climber {
+public class Climber extends Subsystem{
     private final Solenoid leftNeutralHookActuator;
     private final Solenoid rightNeutralHookActuator;
 
@@ -18,6 +18,7 @@ public class Climber {
     public Climber(Solenoid leftNeutralHookActuator, Solenoid rightNeutralHookActuator,
             Solenoid leftClimbingHookActuator, Solenoid rightClimbingHookActuator, CANSparkMax leftWinchMotor,
             CANSparkMax rightWinchMotor) {
+        super("Climber");
         this.leftNeutralHookActuator = leftNeutralHookActuator;
         this.rightNeutralHookActuator = rightNeutralHookActuator;
         this.leftClimbingHookActuator = leftClimbingHookActuator;
@@ -33,5 +34,61 @@ public class Climber {
         rightWinchMotor.restoreFactoryDefaults();
         rightWinchMotor.setIdleMode(IdleMode.kBrake);
         rightWinchMotor.setInverted(true);
+    }
+
+    @Override
+    public void close() throws Exception {
+        leftNeutralHookActuator.close();
+        rightNeutralHookActuator.close();
+        leftClimbingHookActuator.close();
+        rightClimbingHookActuator.close();
+        leftWinchMotor.close();
+        rightWinchMotor.close();
+        
+    }
+
+    @Override
+    protected void stopMotors() {
+        leftWinchMotor.stopMotor();
+        rightWinchMotor.stopMotor();
+    }
+
+
+    public void rotateLeftClimbingHookVertical() {
+        leftClimbingHookActuator.set(true);
+    }
+
+    public void rotateRightClimbingHookVertical() {
+        rightClimbingHookActuator.set(true);
+    }
+    public void rotateLeftClimbingHookDown() {
+        leftClimbingHookActuator.set(false);
+    }
+
+    public void rotateRightClimbingHookDown() {
+        rightClimbingHookActuator.set(false);
+    }
+
+
+    public void rotateLeftNeutralHookVertical() {
+        leftNeutralHookActuator.set(true);
+    }
+
+    public void rotateRightNeutralHookVertical() {
+        rightNeutralHookActuator.set(true);
+    }
+    public void rotateLeftNeutralHookDown() {
+        leftNeutralHookActuator.set(false);
+    }
+
+    public void rotateRightNeutralHookDown() {
+        rightNeutralHookActuator.set(false);
+    }
+
+    public void extendArm(int distance) {
+        //TODO
+    }
+    public void retractArm(int distance) {
+        //TODO
     }
 }
