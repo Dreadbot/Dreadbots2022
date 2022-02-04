5b4ac67b19ec1b52849c10926b9d6f9b665e995a
= <=>
2022-02-03 20:17:31 -0500

    Fisheye wrapper complete, ready for merge with dev Fisheye fully commented and reworked for readability

--- vision/tracking/scratch.py
+++ vision/tracking/scratch.py
@@ -2,80 +2,67 @@ import cv2
 import numpy as np
 import random
 import dreadbot_fisheye
+import math
 
 DIM = [480,640,3]
 
-img = np.zeros(DIM, dtype=np.uint8)
+right = np.zeros(DIM, dtype=np.uint8)
+left = np.zeros(DIM, dtype=np.uint8)
 
-p1_x, p1_y, p1_x2, p1_y2 = (50,50,100,100)
+right[::] = [100,33,120]
+left[::] = [93,120,25]
 
-p2_x, p2_y, p2_x2, p2_y2 = (75,75,125,125)
+ol_width = []
 
-gray = np.zeros([50,50,3], dtype=np.uint8)
-# gray[::] = [180,56,101]
-h, w, d = gray.shape
-color_step = 255/w
+gap = 30
 
-
-light_gray = np.zeros([50,50,3], dtype=np.uint8)
-# light_gray[::] = [201,160,91]
-r = 0
-g = 0
-for col in range(w):
-    r += color_step
-    print(r)
-    g += color_step
-    gray[col:col+1, 0:h] = [50,g,100]
-    light_gray[col:col+1, 0:h] = [r,50,100]
-
-
-overlap_x1, overlap_y1, overlap_x2, overlap_y2 = (75,75,100,100)
-
-img[p1_y:p1_y2, p1_x:p1_x2] = gray
-img[p2_y:p2_y2, p2_x:p2_x2] = light_gray
-
-ol_gray = gray[25:50, 25:50]
-ol_lgray = light_gray[0:25, 0:25]
-
-ol_width = 25
-regions = 5
-region_width = ol_width//regions
-# print("Region Width: {0}".format(region_width))
-
-a = 1
-b = 0
-
-weight_steps = 1/regions
-
-# print(weight_steps)
-
-for r in range(regions):
-    gray_overlap = gray[0:25, r*region_width:(r+1)*region_width]
-    light_gray_overlap = gray[0:25, r*region_width:(r+1)*region_width]
-
-    img_slice = img[overlap_y1:overlap_y2, overlap_x1+(region_width*r):overlap_x1+(region_width*(r+1))]
-
-    # print("Gray Shape:{0}\nLight Gray Shape:{1}\nRegion Shape:{2}".format(gray_overlap.shape, light_gray_overlap.shape, img_slice.shape))
-
-
-    a -= weight_steps
-    b += weight_steps
-
-    a = round(a, 1)
-    b = round(b, 1)
-
-    gray_weighted = np.multiply(gray_overlap, a)
-    light_gray_weighted = np.multiply(light_gray_overlap, b)
-
-    print(gray_weighted[0:1, 0:1])
-    print(light_gray_weighted[0:1, 0:1])
+for row in range(480):
+    ol_width.append(int(abs(math.sin(row*0.025)*70) + gap))
 
 
 
-    img[overlap_y1:overlap_y2, overlap_x1+(region_width*r):overlap_x1+(region_width*(r+1))] = np.multiply(gray_overlap, a) + np.multiply(light_gray_overlap, b)
-    # img[overlap_y1:overlap_y2, overlap_x1+(region_width*r):overlap_x1+(region_width*(r+1))] = [0,0,10*r]
-    # print(img[overlap_y1:overlap_y1+1, overlap_x1+(region_width*r):overlap_x1+(region_width*r)+1])
+# output = dreadbot_fisheye.image_blend(left, right, 100)
 
-cv2.imshow('frame', img)
+final_width = 2*640 - max(ol_width)
 
+output = np.zeros([480, final_width, 3], dtype=np.uint8)
+
+_, cx_output, _ = output.shape
+
+cx_output *= 0.5
+
+cx_output = int(cx_output)
+
+for i in range(len(ol_width)):
+    span = ol_width[i]
+
+    left_strip = left[i:i+1, 640-span:640]
+    right_strip = right[i:i+1, 0:span]
+
+    transparency_step = 1/(2*span)
+
+    left_weight = 1
+    right_weight = 0
+
+    output_strip = np.zeros([1, 2*span, 3], dtype=np.uint8)
+
+    strip_width = 2*span
+
+    for col in range(span*2):
+        left_piece = left_strip[0:1, span-col:span-col+1]
+        right_piece = right_strip[0:1, col:col+1]
+        # print(left_piece.shape)
+        print(right_piece.shape)
+        # if left_piece.any() is None:
+        #     left_piece = np.zeros([1, span, 3], dtype=np.uint8)
+        # if right_piece.any() is None:
+        #     right_piece = np.zeros([1, span, 3], dtype=np.uint8)
+        # output_strip[i:i+1, strip_width+col:strip_width+col+1] = cv2.addWeighted(left_piece, left_weight, right_piece, right_weight, 0)
+        output_strip[i:i+1, strip_width+col:strip_width+col+1] = [255,255,255]
+        left_weight -= transparency_step
+        right_weight += transparency_step
+    output[i:i+1, cx_output-span:cx_output+span] = output_strip
+
+
+cv2.imshow('frame',output)
 cv2.waitKey(0)
--- vision/tracking/test_calibration.py
+++ vision/tracking/test_calibration.py
@@ -4,8 +4,9 @@ import dreadbot_fisheye
 from enum import Enum
 
 
-#True for camera, False for image
+# True for camera, False for image
 src = True
+
 
 class CameraMode(Enum):
     IMAGE = 0
@@ -19,23 +20,24 @@ class FunctionMode(Enum):
     HOMOGRAPHY_SHOW = 2
     TAKE_IMG = 3
 
+
 GLOBAL_X = 640
 GLOBAL_Y = 480
 
 ####################################################    MODE SET #######################################
 camera_mode = CameraMode.DUAL_FISHEYE
-function_mode = FunctionMode.CAT_FISHEYE
+function_mode = FunctionMode.TAKE_IMG
 ########################################################################################################
 
 
 if camera_mode == CameraMode.SINGLE_FISHEYE or camera_mode == CameraMode.DUAL_FISHEYE:
     print("Fisheye ID 1 Enabled")
     fisheye_0 = dreadbot_fisheye.Fisheye(1, 0)
-    
+
 if camera_mode == CameraMode.DUAL_FISHEYE:
     print("Fisheye ID 2 Enabled")
     fisheye_1 = dreadbot_fisheye.Fisheye(2, 1)
-    
+
 if camera_mode == CameraMode.IMAGE:
     print("Image loaded")
     img = cv2.imread('0.png')
@@ -65,34 +67,50 @@ while function_mode == FunctionMode.CAT_FISHEYE:
         undistorted_img_0 = cv2.imread('undistorted.png')
         undistorted_img_1 = cv2.imread('distorted.png')
 
-    calibrated_point = (87,255)
-    testing_point = (207,255)
+    horizontal_y = 260
 
-    c_x, c_y = calibrated_point
-    t_x , t_y = testing_point
+    vertical_x = 330
 
-    _cx, _cy = (320,240)
+    horizontal_calibrated_point = (87, horizontal_y)
+    horizontal_testing_point = (207, horizontal_y)
 
-    f_c_x, f_c_y = fisheye_0.reverse_project_point(c_x, c_y)
-    f_t_x, f_t_y = fisheye_0.reverse_project_point(t_x, t_y)
-    _f_cx, _f_cy = fisheye_0.reverse_project_point(_cx, _cy)
-    print("Fixed Center: ({0}, {1})".format(_f_cx, _f_cy))
-    print("Fixed Calibration: ({0}, {1})\nFixed Test: ({2}, {3})".format(f_c_x, f_c_y, f_t_x, f_t_y))
-    
-    print("Calculated Angle: {0} degrees".format(fisheye_0.calculate_angle(t_x, t_y)))
-    break
-    
-    cv2.line(undistorted_img_0, (0,240), (640, 240), (0,0,255)) #CX LINE
-    cv2.line(undistorted_img_0, (320,0), (320, 480), (0,0,255)) #CX LINE
+    vertical_calibrated_point = (vertical_x, 18)
+    vertical_testing_point = (vertical_x, 118)
 
-    cv2.line(undistorted_img_0, (c_x, 0), (c_x,480), (0,255,0)) #X Calibrated Line up
-    cv2.line(undistorted_img_0, (t_x, 0), (t_x,480), (0,255,0)) #X Calibrated Line up
+    v_c_x, v_c_y = vertical_calibrated_point
+    v_t_x, v_t_y = vertical_testing_point
 
-    cv2.line(undistorted_img_0, (0, c_y), (640, c_y), (0,255,0)) #Y Line up
+    h_c_x, h_c_y = horizontal_calibrated_point
+    h_t_x, h_t_y = horizontal_testing_point
 
-    cv2.circle(undistorted_img_0, calibrated_point, 3, (0,255,0), thickness=-1)
-    cv2.circle(undistorted_img_0, testing_point, 3, (0,255,0), thickness=-1)
+    cx, cy = (320, 240)
 
+    h_f_c_x, h_f_c_y = fisheye_0.reverse_project_point(h_c_x, h_c_y)
+    h_f_t_x, h_f_t_y = fisheye_0.reverse_project_point(h_t_x, h_t_y)
+
+    v_f_c_x, v_f_c_y = fisheye_0.reverse_project_point(v_c_x, v_c_y)
+    v_f_t_x, v_f_t_y = fisheye_0.reverse_project_point(v_t_x, v_t_y)
+
+
+    h__f_cx, h__f_cy = fisheye_0.reverse_project_point(cx, cy)
+
+    cv2.line(undistorted_img_0, (0, 240), (640, 240), (0, 0, 255))  # CX LINE
+    cv2.line(undistorted_img_0, (320, 0), (320, 480), (0, 0, 255))  # CY LINE
+
+    cv2.line(undistorted_img_0, (h_c_x, 0), (h_c_x, 480), (0, 255, 0))  # X Calibrated Line up
+    cv2.line(undistorted_img_0, (h_t_x, 0), (h_t_x, 480), (0, 255, 0))  # X Testing Line up
+
+    cv2.line(undistorted_img_0, (0, v_c_y), (640, v_c_y), (255, 0, 0))  # Y Calibrated Line Up
+    cv2.line(undistorted_img_0, (0, v_t_y), (640, v_t_y), (255, 0, 0))  # Y Testing Line Up
+
+    cv2.line(undistorted_img_0 ,(vertical_x, 0), (vertical_x, 480), (255,0,0)) # X Line Up
+    cv2.line(undistorted_img_0, (0, h_c_y), (640, h_c_y), (0, 255, 0))  # Y Line Up
+
+    cv2.circle(undistorted_img_0, horizontal_calibrated_point, 3, (0, 255, 0), thickness=-1)
+    cv2.circle(undistorted_img_0, horizontal_testing_point, 3, (0, 255, 0), thickness=-1)
+
+    cv2.circle(undistorted_img_0, vertical_calibrated_point, 3, (255,0,0), thickness=-1)
+    cv2.circle(undistorted_img_0, vertical_testing_point, 3, (255,0,0), thickness=-1)
 
     cat_img = np.concatenate((undistorted_img_0, undistorted_img_1), axis=1)
 
@@ -116,11 +134,11 @@ while function_mode == FunctionMode.HOMOGRAPHY_SHOW:
         undistorted_img_1 = fisheye_0.ret_undist()
         undistorted_img_0 = fisheye_1.ret_undist()
 
-    mixed_img = dreadbot_fisheye.image_blend(undistorted_img_0, undistorted_img_1, 25, blend_mode=dreadbot_fisheye.LINEAR)
-    
+    mixed_img = dreadbot_fisheye.image_blend(
+        undistorted_img_0, undistorted_img_1, 25, blend_mode=dreadbot_fisheye.LINEAR)
 
     cv2.imshow('frame', mixed_img)
-    
+
     # cv2.waitKey(0)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
@@ -132,7 +150,6 @@ while function_mode == FunctionMode.TAKE_IMG:
     cv2.imwrite("undistorted.png", fisheye_0.ret_undist())
     cv2.imwrite("distorted.png", fisheye_0.ret_dist())
     break
-
 
 
 # while True:
