317fc73fa0c003fa91a61e594e8b02fe5dfe701b
Odog0 <c3pooliver@gmail.com>
2022-02-14 18:26:37 -0500

    add color sensor

--- robot/src/main/java/frc/robot/subsystem/Intake.java
+++ robot/src/main/java/frc/robot/subsystem/Intake.java
@@ -5,12 +5,38 @@
 package frc.robot.subsystem;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ColorSensorV3;
 
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
     private final CANSparkMax motor;
+
+    private final I2C.Port i2cPort = I2C.Port.kOnboard;
+    private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+
+    public boolean isballblue() {
+        Color detectedColor = m_colorSensor.getColor();
+        double IR = m_colorSensor.getIR();
+        double red1 = detectedColor.red;
+        double green1 = detectedColor.green;
+        double blue1 = detectedColor.blue;
+        SmartDashboard.putNumber("Red", detectedColor.red);
+        SmartDashboard.putNumber("Green", detectedColor.green);
+        SmartDashboard.putNumber("Blue", detectedColor.blue);
+        SmartDashboard.putNumber("IR", IR);
+        if (red1>blue1){
+            return false;
+        }
+        else {
+            return true;
+        }
+        
+    }
 
     public Intake(CANSparkMax motor) {
         this.motor = motor;
