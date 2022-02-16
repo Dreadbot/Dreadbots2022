c63f371cd425c621f8afc4d931e0077e24c83ff5
cjohnson <collin.g.johnson@gmail.com>
2022-02-16 12:17:38 -0500

    Overhaul subsystem base class

---
+++ robot/src/main/java/frc/robot/subsystem/DreadbotSubsystem.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystem;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.util.MotorSafeSystem;
+
+public abstract class DreadbotSubsystem extends SubsystemBase implements AutoCloseable, MotorSafeSystem {
+    private boolean enabled = true;
+
+    protected DreadbotSubsystem() { }
+
+    public void disable() {
+        if(!enabled) return;
+
+        this.enabled = false;
+    }
+
+    public boolean isDisabled() {
+        return !enabled;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
