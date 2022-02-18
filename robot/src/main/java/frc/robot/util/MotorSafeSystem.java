e00545f410d50781d8f3d1acadeb91fcea3a8b9f
cjohnson <collin.g.johnson@gmail.com>
2022-02-18 00:26:59 -0500

    Add docs for MotorSafeSystem

--- robot/src/main/java/frc/robot/util/MotorSafeSystem.java
+++ robot/src/main/java/frc/robot/util/MotorSafeSystem.java
@@ -1,5 +1,12 @@
 package frc.robot.util;
 
+/**
+ * An object that manages hardware resources (such as {@link com.revrobotics.CANSparkMax}).
+ * Any hardware managing object should have a method to completely stop mechanical functions.
+ */
 public interface MotorSafeSystem {
+    /**
+     * Stops all hardware functions handled by this class.
+     */
     void stopMotors();
 }
