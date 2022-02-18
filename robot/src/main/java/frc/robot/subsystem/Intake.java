882819c227dbbdf84fb637c40c41c0c27ae91a8d
cjohnson <collin.g.johnson@gmail.com>
2022-02-17 23:17:23 -0500

    Convert intake to DreadbotSubsystem class and fix unit tests to read motor values directly

--- robot/src/main/java/frc/robot/subsystem/Intake.java
+++ robot/src/main/java/frc/robot/subsystem/Intake.java
@@ -5,15 +5,12 @@
 package frc.robot.subsystem;
 
 import com.revrobotics.CANSparkMax;
-
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.util.MotorSafeSystem;
 
 /**
  * The intake is the mechanism that takes cargo from the ground into the feeder mechanism.
  */
-public class Intake extends SubsystemBase implements AutoCloseable, MotorSafeSystem {
+public class Intake extends DreadbotSubsystem {
     private final CANSparkMax motor;
 
     public Intake(CANSparkMax motor) {
@@ -21,9 +18,14 @@ public class Intake extends SubsystemBase implements AutoCloseable, MotorSafeSys
 
         // Immediately close motors if subsystem is disabled.
         if(!Constants.INTAKE_ENABLED) {
+            disable();
             motor.close();
+
             return;
         }
+
+        motor.restoreFactoryDefaults();
+        motor.setIdleMode(CANSparkMax.IdleMode.kCoast);
 
         // Motor is inverted because by default a positive value represents motion
         // that moves the ball out of the intake.
@@ -35,11 +37,12 @@ public class Intake extends SubsystemBase implements AutoCloseable, MotorSafeSys
      */
     public void intake() {
         if(!Constants.INTAKE_ENABLED) return;
+        if(isDisabled()) return;
 
         // Set the motor to a high positive speed.
         try {
             motor.set(1.0d);
-        } catch (IllegalStateException ignored) { /* No stack trace */ }
+        } catch (IllegalStateException ignored) { disable(); }
     }
 
     /**
@@ -47,11 +50,12 @@ public class Intake extends SubsystemBase implements AutoCloseable, MotorSafeSys
      */
     public void outtake() {
         if(!Constants.INTAKE_ENABLED) return;
+        if(isDisabled()) return;
 
         // Set the motor to a high negative speed.
         try {
             motor.set(-1.0d);
-        } catch (IllegalStateException ignored) { /* No stack trace */ }
+        } catch (IllegalStateException ignored) { disable(); }
     }
 
     /**
@@ -59,58 +63,60 @@ public class Intake extends SubsystemBase implements AutoCloseable, MotorSafeSys
      */
     public void idle() {
         if(!Constants.INTAKE_ENABLED) return;
+        if(isDisabled()) return;
 
         // Set the motor to zero movement.
         try {
             motor.set(0.0d);
-        } catch (IllegalStateException ignored) { /* No stack trace */ }
+        } catch (IllegalStateException ignored) { disable(); }
     }
 
     public boolean isIntaking() {
         if(!Constants.INTAKE_ENABLED) return false;
+        if(isDisabled()) return false;
 
         // Get the current commanded speed. If there is a failure,
         // the output is considered zero.
         double output = 0.0d;
         try {
             output = motor.get();
-        } catch (IllegalStateException ignored) { /* No stack trace */ }
+        } catch (IllegalStateException ignored) { disable(); }
 
         return output > 0.0d;
     }
 
     public boolean isOuttaking() {
         if(!Constants.INTAKE_ENABLED) return false;
+        if(isDisabled()) return false;
 
         // Get the current commanded speed. If there is a failure,
         // the output is considered zero.
         double output = 0.0d;
         try {
             output = motor.get();
-        } catch (IllegalStateException ignored) { /* No stack trace */ }
+        } catch (IllegalStateException ignored) { disable(); }
 
         return output < 0.0d;
     }
 
     @Override
-    public void close() {
+    public void stopMotors() {
         if(!Constants.INTAKE_ENABLED) return;
+        if(isDisabled()) return;
 
+        // Use the built-in motor stop method.
+        try {
+            motor.stopMotor();
+        } catch (IllegalStateException ignored) { disable(); }
+    }
+
+    @Override
+    public void close() {
         // Stop motor before closure.
         stopMotors();
 
         try {
             motor.close();
-        } catch (IllegalStateException ignored) { /* No stack trace */ }
-    }
-
-    @Override
-    public void stopMotors() {
-        if(!Constants.INTAKE_ENABLED) return;
-
-        // Use the built-in motor stop method.
-        try {
-            motor.stopMotor();
-        } catch (IllegalStateException ignored) { /* No stack trace */ }
+        } catch (IllegalStateException ignored) { disable(); }
     }
 }
--- robot/src/test/java/frc/robot/subsystem/IntakeTest.java
+++ robot/src/test/java/frc/robot/subsystem/IntakeTest.java
@@ -2,13 +2,12 @@ package frc.robot.subsystem;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
+import edu.wpi.first.hal.HAL;
+import frc.robot.Constants;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import edu.wpi.first.hal.HAL;
-import frc.robot.Constants;
+import org.junit.Test.None;
 
 import static org.junit.Assert.*;
 
@@ -33,47 +32,51 @@ public class IntakeTest {
 
     @Test
     public void intake() {
-        if(!Constants.INTAKE_ENABLED) return;
-
+        System.out.println("intake.isDisabled() = " + intake.isDisabled());
         intake.intake();
 
-        assertEquals(1.0d, intakeMotor.get(), DELTA);
+        System.out.println("intake.isDisabled() = " + intake.isDisabled());
+        if (intake.isEnabled()) {
+            assertEquals(1.0d, intakeMotor.get(), DELTA);
+        }
     }
 
     @Test
     public void outtake() {
-        if(!Constants.INTAKE_ENABLED) return;
-
         intake.outtake();
 
-        assertEquals(-1.0d, intakeMotor.get(), DELTA);
+        if (intake.isEnabled()) {
+            assertEquals(-1.0d, intakeMotor.get(), DELTA);
+        }
     }
 
     @Test
     public void idle() {
-        if(!Constants.INTAKE_ENABLED) return;
-
         intake.idle();
 
-        assertEquals(0.0, intakeMotor.get(), DELTA);
+        if (intake.isEnabled()) {
+            assertEquals(0.0, intakeMotor.get(), DELTA);
+        }
     }
 
     @Test
     public void stopMotors() {
-        if(!Constants.INTAKE_ENABLED) return;
-
         intake.stopMotors();
 
-        assertEquals(0.0d, intakeMotor.get(), DELTA);
+        if (intake.isEnabled()) {
+            assertEquals(0.0d, intakeMotor.get(), DELTA);
+        }
     }
 
     @SuppressWarnings("SpellCheckingInspection")
     @Test
     public void isIntaking() {
-        if(!Constants.INTAKE_ENABLED) return;
-
         intake.intake();
-        assertTrue(intake.isIntaking());
+        if (intake.isEnabled()) {
+            assertTrue(intake.isIntaking());
+        } else {
+            assertFalse(intake.isIntaking());
+        }
 
         intake.idle();
         assertFalse(intake.isIntaking());
@@ -82,20 +85,20 @@ public class IntakeTest {
     @SuppressWarnings("SpellCheckingInspection")
     @Test
     public void isOuttaking() {
-        if(!Constants.INTAKE_ENABLED) return;
-
         intake.outtake();
-        assertTrue(intake.isOuttaking());
+        if (intake.isEnabled()) {
+            assertTrue(intake.isOuttaking());
+        } else {
+            assertFalse(intake.isOuttaking());
+        }
 
         intake.idle();
         assertFalse(intake.isOuttaking());
     }
 
     @SuppressWarnings("DefaultAnnotationParam")
-    @Test(expected = Test.None.class /* No exception should be thrown */)
+    @Test(expected = None.class /* No exception should be thrown */)
     public void close() {
-        if(!Constants.INTAKE_ENABLED) return;
-
         intake.close();
 
         // Despite making calls to closed objects, these functions should not
