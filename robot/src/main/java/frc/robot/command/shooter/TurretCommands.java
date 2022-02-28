f1f356d8421d09df12efc669ccf24e47d966c493
cjohnson <collin.g.johnson@gmail.com>
2022-02-28 17:32:03 -0500

    Add external pid tracking id

--- robot/src/main/java/frc/robot/command/shooter/TurretCommands.java
+++ robot/src/main/java/frc/robot/command/shooter/TurretCommands.java
@@ -1,5 +1,6 @@
 package frc.robot.command.shooter;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystem.shooter.Turret;
@@ -7,12 +8,15 @@ import frc.robot.util.VisionInterface;
 
 public class TurretCommands {
     public static class TurretTrackingCommand extends CommandBase {
+        private final PIDController visionErrorPIDController;
         private final Turret turret;
 
         private double lastRelativeAngleToHub;
 
         public TurretTrackingCommand(Turret turret) {
             this.turret = turret;
+            this.visionErrorPIDController = new PIDController(1.0d, 0.0d, 0.0d);
+            SmartDashboard.putData("VISION TURRET PID", visionErrorPIDController);
 
             addRequirements(turret);
         }
@@ -31,7 +35,11 @@ public class TurretCommands {
 
             // Calculate the commanded absolute angle from relative
             double currentTurretAngle = turret.getAngle();
-            double requestedAngle = currentTurretAngle - relativeAngleToHub;
+            double finalAngle = currentTurretAngle - relativeAngleToHub;
+
+            visionErrorPIDController.setSetpoint(finalAngle);
+            double requestedAngle = visionErrorPIDController.calculate(currentTurretAngle);
+
             SmartDashboard.putNumber("DEBUG REQA", requestedAngle);
 
             // Command hardware and update state
