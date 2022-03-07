6a71e29e3a8b007d8178094066d6c8e286cd7933
cjohnson <collin.g.johnson@gmail.com>
2022-03-07 17:11:29 -0500

    Add vision interface comments

--- robot/src/main/java/frc/robot/util/VisionInterface.java
+++ robot/src/main/java/frc/robot/util/VisionInterface.java
@@ -1,40 +1,29 @@
 package frc.robot.util;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.Constants;
 
+/**
+ * Provides an API for accessing vision processing outputs from NetworkTables.
+ */
 public class VisionInterface {
+    /**
+     * Gets the vision calculated distance (in inches) from the vision calculations on the pi.
+     * Returns -1.0d if the value isn't found on the NetworkTables.
+     *
+     * @return Calculated distance from the hub (inches)
+     */
     public static double getRelativeDistanceToHub() {
         return SmartDashboard.getNumber("RelativeDistanceToHub", -1.0d);
     }
 
+    /**
+     * Gets the vision calculated relative angle (in degrees) from the vision calculations on the pi.
+     * Returns Double.MAX_VALUE if the value isn't found on the NetworkTables.
+     *
+     * @return Calculated relative angle to the hub (degrees)
+     */
     public static double getRelativeAngleToHub() {
         return SmartDashboard.getNumber("RelativeAngleToHub", Double.MAX_VALUE);
-    }
-
-    public static double getFlywheelVelocity(boolean correctBallColor) {
-        if (correctBallColor) {
-            return SmartDashboard.getNumber("Requested Flywheel RPM", 0.0d);
-        } else {
-            // Wrong Color, just eject the ball
-            return 500;
-        }
-    }
-
-    public static double getRequestedTurretAngle(boolean correctBallColor) {
-        if(correctBallColor){
-            return SmartDashboard.getNumber("Requested Turret Angle", 0.0d);
-        } else {
-            return SmartDashboard.getNumber("Requested Turret Angle", 0.0d) + 10.0;
-        }
-    }
-
-    public static double getRequestedHoodAngle(boolean correctBallColor) {
-        if(correctBallColor){
-            return SmartDashboard.getNumber("Requested Hood Angle", 0.0d);
-        } else {
-            return Constants.MAX_HOOD_ANGLE - 10.0d;
-        }
     }
 
     public static void selectCamera(int camera) {
@@ -43,12 +32,6 @@ public class VisionInterface {
 
     public static boolean canTrackHub() {
         return SmartDashboard.getBoolean("TargetFoundInFrame", false);
-    }
-
-    public static void debug() {
-        SmartDashboard.putNumber("DEBUG DISTANCE", getRelativeDistanceToHub());
-        SmartDashboard.putNumber("DEBUG ANGLE", getRelativeAngleToHub());
-        SmartDashboard.putBoolean("DEBUG CANTRACK", canTrackHub());
     }
 
     private VisionInterface() {
