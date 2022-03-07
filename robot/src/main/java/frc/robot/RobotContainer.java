a2cb1321c0e64b669990339f2d63d479ec513923
cjohnson <collin.g.johnson@gmail.com>
2022-03-07 11:41:36 -0500

    Remove obsolete shooter classes

--- robot/src/main/java/frc/robot/RobotContainer.java
+++ robot/src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.command.autonomous.VelocityControlTestCommand;
 import frc.robot.command.climber.*;
