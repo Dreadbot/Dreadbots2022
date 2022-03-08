2aa24436aa1de29b26a8181f535fad338a73c23b
= <=>
2022-03-08 11:00:28 -0500

    update push

--- vision/turret_pi/main.py
+++ vision/turret_pi/main.py
@@ -10,6 +10,9 @@ import projection_math
 from cscore import CameraServer
 import time, sys, logging
 import numpy as np
+import rw_visualizer
+
+import time
 
 dataDir = os.path.join("vision", "Targeting", "Data")
 
@@ -95,6 +98,9 @@ def resImg(frame):
     resFrame = cv2.resize(frame, (dstW, dstH))
     return resFrame
 
+def pt_sort(e):
+    return(e[0])
+
 def main():
     argparser = argparse.ArgumentParser()
     argparser.add_argument('-p', '--data-path',
@@ -129,24 +135,32 @@ def main():
 
     cs = cv2.VideoCapture(0)
 
-    cs.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
-    cs.set(cv2.CAP_PROP_EXPOSURE, -15)
+    cs.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.75)
+    cs.set(cv2.CAP_PROP_EXPOSURE, 0.01)
 
     focalLength = 667
 
     if table is not None:
-        table.putNumber("TargetHLowerValue", 54)
-        table.putNumber("TargetHUpperValue", 74)
-        table.putNumber("TargetLLowerValue", 234)
+        table.putNumber("TargetHLowerValue", 50)
+        table.putNumber("TargetHUpperValue", 150)
+        table.putNumber("TargetLLowerValue", 100)
         table.putNumber("TargetLUpperValue", 255)
-        table.putNumber("TargetSLowerValue", 139)
+        table.putNumber("TargetSLowerValue", 200)
         table.putNumber("TargetSUpperValue", 255)
         table.putNumber("CameraSelection", 1)
         table.putNumber("DampenWeight", 1)
+        table.putNumber("YIgnore", 1)
+
 
     dampen_weight = table.getNumber("DampenWeight", 1)
 
-    prev_dist = 0
+    rw_vis = rw_visualizer.space(800,800)
+
+    prev_angle = 0
+    prev_distance = 0
+
+    angle = 0
+    distance = 0
 
     while True:
 
@@ -161,9 +175,10 @@ def main():
         hue = [table.getNumber("TargetHLowerValue", 0), table.getNumber("TargetHUpperValue", 255)]
         sat = [table.getNumber("TargetSLowerValue", 0), table.getNumber("TargetSUpperValue", 255)]
         lum = [table.getNumber("TargetLLowerValue", 0), table.getNumber("TargetSUpperValue", 255)]
-        blur = int(table.getNumber("TurretBlurKernel", 1))
+        blur = max([int(table.getNumber("TurretBlurKernel", 1)), 1])
         erode = int(table.getNumber("TurretErodeKernel", 1))
         dilate = int(table.getNumber("TurretErodeKernel", 1))
+        y_ignore = int(table.getNumber("YIgnore", 0))
         data = getData()
         #(hL, sL, vL) = data["lower"]
         #(hU, sU, vU) = data["upper"]
@@ -175,92 +190,115 @@ def main():
         cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL,
                                 cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
-
+        frame[::] += 50
         xs = []
         ys = []
 
         true_xs = []
         true_ys = []
 
+        scale_step = 30
+
+        cv2.line(frame, (0, y_ignore), (640, y_ignore), (255,255,255))
+        # print("---")
         for c in cnts:
             x, y, w, h = cv2.boundingRect(c)
+
+            if y >= y_ignore:
+                continue
 
             x += (w//2)
             y += (h//2)
             # cv2.putText(mask, str(p), (x + 30, y + 30),
             #             cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2, cv2.LINE_AA)
-            if 5 < w < 30 and 5 < h < 15:
-                # cv2.circle(frame, (x, y), 3, (0,0,255), thickness=-1)
+            if 0 < w < 30 and 0 < h < 15:
+                cv2.circle(frame, (x, y), 3, (0,0,255), thickness=-1)
                 xs.append(x)
                 ys.append(y)
+                # # wvec = projection_math.pinhole_angle_calculation((x, y), frame, scale_step)
+                # # wvec = projection_math.camera_to_world(x, y)
+                # wvec = projection_math.similar_triangles_calculation(x, y)
+                # u, v = projection_math.reverse_point(wvec, round=True)
+                # cvec = (u, v)
+                # # print(cvec)
+                # test_wvec = projection_math.pinhole_angle_calculation((x,y), frame, scale_step)
+                # # print(f"u,v[ {x, y} ]   wvec[ {test_wvec[::]} ]  triangle_wvec[ {wvec[::]} ]")
+                # print(wvec)
+                # cv2.circle(frame, cvec, 5, (0,255,255), thickness=-1)
+                # scale_step += 15
+                # # time.sleep(0.5) #REMOVE THIS LATER
+
+                cv2.circle(frame, (x, y), 3, (0,0,0), thickness=-1)
 
         center_ret = False
         y_range = 15
-        x_range_l = 0
-        x_range_u = 100
+        x_range = 50
         add_cnt = 2
         
-        # print(len(xs))
-        for p in range(len(xs)):
-            px, py = (xs[p], ys[p])
-
-            cur_add = 0
-            for t in range(len(xs)):
-                ty = ys[t]
-                tx = xs[t]
-
-                diff = abs(py-ty)
-                x_diff = abs(px-tx)
-
-                if diff <= y_range and diff != 0 and x_diff < x_range_u:
-                    # print("fits")
-                    cur_add += 1
-
-            
-            if cur_add >= add_cnt:
-                true_xs.append(px)
-                true_ys.append(py)
-                    
-        # print(f"{true_xs} {true_ys} \n")
-        true_xs = xs
-        true_ys = ys
-        #for p in range(len(true_xs)):
-        #    point = (true_xs[p], true_ys[p])
-        #    cv2.circle(frame, point, 3, (255,0,0), thickness=-1)
         
-        if len(xs) > 1:
-            p1 = (xs[0], ys[0])
-            p2 = (xs[1], ys[1])
-            cv2.circle(frame, p1, 3, (0,0,255), thickness=-1)
-            cv2.circle(frame, p2, 3, (0,0,255), thickness=-1)
-            center_ret, angle, distance = projection_math.geometric_true_center(p1, p2, draw_target=frame)
-            if center_ret:
-                cur_dist = dampen_weight*distance + (1-dampen_weight)*prev_dist
-                prev_dist = cur_dist
-        if not center_ret:
-            angle = 0
-            distance = 0
+#        for point in range(len(xs)):
+#            px, py = xs[point], ys[point]
+#            
+#            cv2.circle(frame, (px, py), 3, (0,255,255), thickness=-1)
+#            cv2.line(frame, (px-x_range, py), (px+x_range, py), (0,255,255), thickness=3)
+        imgpoints = []
+        for i in range(len(xs)):
+            imgpoints.append([xs[i], ys[i]])
+        
+        imgpoints.sort(reverse=True, key=pt_sort)
+
+        for pt in imgpoints:
+            u, v = pt
+            x, y, z = projection_math.similar_triangles_calculation(u, v)
+            rw_vis.circle((x,y), (0,0,255))
+
+        if len(xs) > 2:
+            # angle, distance = projection_math.point_center(xs, ys, frame)
+            # p1 = (xs[0], ys[0])
+            # p2 = (xs[1], ys[1])
+            # center_ret, angle, distance = projection_math.geometric_true_center(p1, p2, draw_target=frame)
+            cvec = projection_math.orth_bisector_calculation(imgpoints, draw_target=frame, visualizer=rw_vis)
+
+            # rw_vis.circle(cvec, (0,255,0))
+            cv2.circle(frame, cvec, 5, (255,0,0), thickness=-1)
+
+            table.putBoolean("TargetFoundInFrame", True)
+        else:
             table.putBoolean("TargetFoundInFrame", False)
 
+            # cv2.circle(frame, (int(ax), int(ay)), 3, (123,213,150), thickness=-1)
+        
+        # center_ret = (angle is not 0)
+
+        # if not center_ret:
+        #     angle = 0
+        #     distance = 0
+        #     table.putBoolean("TargetFoundInFrame", False)
+
         if table is not None:
-            table.putBoolean("TargetFoundInFrame", center_ret)
-            if center_ret:
-                table.putNumber("RelativeDistanceToHub", cur_dist)
-                table.putNumber("RelativeAngleToHub", angle)
+            # table.putBoolean("TargetFoundInFrame", center_ret)
+            # if center_ret:
+            table.putNumber("RelativeDistanceToHub", distance)
+            table.putNumber("RelativeAngleToHub", angle)
 
         if cs is not None:
             # frame = np.concatenate((frame, mask), axis=1)
             # frame = mask
+            # frame[::] += 50
             if table.getNumber("CameraSelection", 0) == 0:
                 cv2.line(frame, (320, 0), (320,480), (0,0,255))
                 frame = resImg(frame)
                 outputStream.putFrame(frame)
             elif table.getNumber("CameraSelection", 0) == 1:
-                # mask = resImg(mask)
                 mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
                 mask = cv2.addWeighted(mask, 0.8, frame, 0.2, 0.0)
-                cv2.line(mask, (320,0), (320,480), (0,0,255))
+                mask = resImg(mask)
+                w, h, _ = mask.shape
+                cv2.line(mask, (w//2,0), (w//2,h), (0,0,255))
                 outputStream.putFrame(mask)
+            elif table.getNumber("CameraSelection", 0) == 2:
+                frame = rw_vis.retrieve_img()
+                outputStream.putFrame(frame)
 
         # cv2.imshow("Frame", frame)
         # cv2.imshow("Binary", mask)
---
+++ vision/turret_pi/start.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+/usr/bin/python3 /home/pi/vision/Targeting/main.py -p /home/pi/vision/Targeting/Data
