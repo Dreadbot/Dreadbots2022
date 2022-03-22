9e7a43acd80c525768cac68992715de0aae02571
The-No-rush <ethan.norush@protonmail.com>
2022-03-22 17:41:55 -0400

    Auton climb now stops when button isn't held

--- robot/src/main/java/frc/robot/command/climber/AutonomousClimberCommand.java
+++ robot/src/main/java/frc/robot/command/climber/AutonomousClimberCommand.java
@@ -1,6 +1,5 @@
 package frc.robot.command.climber;
 
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
